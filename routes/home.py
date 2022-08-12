ecfc255e1f6096a260e68bf09fb2a78c36ada863
Muhammad <jamiludeensani@gmail.com>
2022-08-12 02:27:51 +0100

    v1.0.0

--- routes/home.py
+++
@@ -1,7 +0,0 @@
-from crypt import methods
-from flask import Blueprint
-from controllers.home import index
-
-home_bp = Blueprint('home_bp', __name__)
-
-home_bp.route('')(index)
--- routes/show.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-from controllers.show import create_show_submission, create_shows, shows
-
-show_bp = Blueprint('show_bp', __name__)
-
-show_bp.get('/')(shows)
-show_bp.get('/create')(create_shows)
-show_bp.post('/create')(create_show_submission)
